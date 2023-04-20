21675ffb3762f148e2072e038701007e574aa289
raulRMB <92171903+raulRMB@users.noreply.github.com>
2023-04-20 19:13:02 -0400

    Initial commit
    Project Setup

---
+++ Source/Radiant/Hero.cpp
@@ -0,0 +1,76 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Hero.h"
+
+#include "RadiantGameModeBase.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+AHero::AHero()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AHero::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		PlayerController->bShowMouseCursor = true;
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(MappingContext, 0);
+		}
+	}
+}
+
+void AHero::Click(const FInputActionValue& Value)
+{
+	// Get the mouse position
+	FVector2D MousePosition;
+	if (GEngine && GEngine->GameViewport)
+	{
+		GEngine->GameViewport->GetMousePosition(MousePosition);
+	}
+
+	// Get the world position
+	FVector WorldPosition;
+	FVector WorldDirection;
+	UGameplayStatics::DeprojectScreenToWorld(GetWorld()->GetFirstPlayerController(), MousePosition, WorldPosition, WorldDirection);
+
+	// Get the hit result
+	FHitResult HitResult;
+	GetWorld()->LineTraceSingleByChannel(HitResult, WorldPosition, WorldPosition + WorldDirection * 10000, ECC_Visibility);
+
+	// Set the destination
+	Destination = HitResult.Location;	
+}
+
+// Called every frame
+void AHero::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	// Move towards the destination
+	FVector dir = Destination - GetActorLocation();
+	dir.Normalize();
+	AddMovementInput(dir, 1);
+}
+
+// Called to bind functionality to input
+void AHero::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if(UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(ClickAction, ETriggerEvent::Started, this, &AHero::Click);
+	}
+}
+
