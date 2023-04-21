e84466e4836f21238d7245e66f5a8833ba78dda6
raulRMB <92171903+raulRMB@users.noreply.github.com>
2023-04-21 15:01:58 -0400

    Added Client Build Target

---
+++ Source/RadiantClient.Target.cs
@@ -0,0 +1,13 @@
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class RadiantClientTarget : TargetRules //Change this line according to the name of your project
+{
+    public RadiantClientTarget(TargetInfo Target) : base(Target) //Change this line according to the name of your project
+    {
+        Type = TargetType.Client;
+        DefaultBuildSettings = BuildSettingsVersion.V2;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+        ExtraModuleNames.Add("Radiant"); //Change this line according to the name of your project
+    }
+}
