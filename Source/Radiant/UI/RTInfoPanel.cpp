1fb81bb46e31b87776c0dd32a824d8404b033933
raulRMB <92171903+raulRMB@users.noreply.github.com>
2023-05-24 16:09:44 -0400

    PlayerInfoPanel - UI

---
+++ Source/Radiant/UI/RTInfoPanel.cpp
@@ -0,0 +1,108 @@
+// Copyright Radiant Studios
+
+
+#include "UI/RTInfoPanel.h"
+
+#include "Character/Hero.h"
+#include "GAS/AbilitySystemComponent/RTAbilitySystemComponent.h"
+#include "Components/ProgressBar.h"
+#include "Components/Image.h"
+#include "Player/RTPlayerState.h"
+
+
+class ARTPlayerState;
+
+void URTInfoPanel::NativeTick(const FGeometry& MyGeometry, float DeltaTime)
+{
+	Super::NativeTick(MyGeometry, DeltaTime);
+
+	UpdateProperties(DeltaTime);
+}
+
+void URTInfoPanel::SetAbilityCoolDown(EAbilityID Abilty, const float Percent)
+{
+	Abilities[static_cast<int>(Abilty)].MaterialInstance->SetScalarParameterValue("Percent", Percent);
+}
+
+void URTInfoPanel::AbilityCooldowns()
+{
+	for(int i = 0; i < Tags.Num(); i++)
+	{
+		float TimeRemaining = 0.f;
+		float CooldownDuration = 0.f;
+		Abilities[i].SetOn(false);
+		if(GetCooldownRemainingForTag(Tags[i], TimeRemaining, CooldownDuration))
+		{
+			float Percent = GetCooldownPercent(TimeRemaining, CooldownDuration);
+			SetAbilityCoolDown(static_cast<EAbilityID>(i), Percent);
+			Abilities[i].SetOn(true);
+		}
+	}
+}
+
+void URTInfoPanel::UpdateProperties(float DeltaTime)
+{
+	URTHeroAttributeSetBase* Attributes = GetOwningPlayerState<ARTPlayerState>()->GetAttributeSetBase();
+
+	AbilityCooldowns();
+
+	HealthBar->SetPercent(Attributes->GetHealth() / Attributes->GetMaxHealth());
+	ManaBar->SetPercent(Attributes->GetMana() / Attributes->GetMaxMana());
+}
+
+void URTInfoPanel::Init()
+{
+	Abilities.Add(FUIAbility(Mat, this, Ability1, Ability1CDMask));
+	Abilities.Add(FUIAbility(Mat, this, Ability2, Ability2CDMask));
+	Abilities.Add(FUIAbility(Mat, this, Ability3, Ability3CDMask));
+	Abilities.Add(FUIAbility(Mat, this, Ability4, Ability4CDMask));
+	Abilities.Add(FUIAbility(Mat, this, Ability5, Ability5CDMask));
+	Abilities.Add(FUIAbility(Mat, this, Ability6, Ability6CDMask));
+
+	Ability1CDMask->SetBrushResourceObject(Abilities[static_cast<int>(EAbilityID::Ability1)].MaterialInstance);
+	Ability2CDMask->SetBrushResourceObject(Abilities[static_cast<int>(EAbilityID::Ability2)].MaterialInstance);
+	Ability3CDMask->SetBrushResourceObject(Abilities[static_cast<int>(EAbilityID::Ability3)].MaterialInstance);
+	Ability4CDMask->SetBrushResourceObject(Abilities[static_cast<int>(EAbilityID::Ability4)].MaterialInstance);
+	Ability5CDMask->SetBrushResourceObject(Abilities[static_cast<int>(EAbilityID::Ability5)].MaterialInstance);
+	Ability6CDMask->SetBrushResourceObject(Abilities[static_cast<int>(EAbilityID::Ability6)].MaterialInstance);
+}
+
+bool URTInfoPanel::GetCooldownRemainingForTag(FGameplayTagContainer CooldownTags, float& TimeRemaining,
+	float& CooldownDuration)
+{
+	URTAbilitySystemComponent* AbilitySystemComponent = Cast<URTAbilitySystemComponent>(GetOwningPlayerState<ARTPlayerState>()->GetAbilitySystemComponent());
+	
+	if (AbilitySystemComponent && CooldownTags.Num() > 0)
+	{
+		TimeRemaining = 0.f;
+		CooldownDuration = 0.f;
+
+		FGameplayEffectQuery const Query = FGameplayEffectQuery::MakeQuery_MatchAnyOwningTags(CooldownTags);
+		TArray< TPair<float, float> > DurationAndTimeRemaining = AbilitySystemComponent->GetActiveEffectsTimeRemainingAndDuration(Query);
+		if (DurationAndTimeRemaining.Num() > 0)
+		{
+			int32 BestIdx = 0;
+			float LongestTime = DurationAndTimeRemaining[0].Key;
+			for (int32 Idx = 1; Idx < DurationAndTimeRemaining.Num(); ++Idx)
+			{
+				if (DurationAndTimeRemaining[Idx].Key > LongestTime)
+				{
+					LongestTime = DurationAndTimeRemaining[Idx].Key;
+					BestIdx = Idx;
+				}
+			}
+
+			TimeRemaining = DurationAndTimeRemaining[BestIdx].Key;
+			CooldownDuration = DurationAndTimeRemaining[BestIdx].Value;
+
+			return true;
+		}
+	}
+
+	return false;
+}
+
+float URTInfoPanel::GetCooldownPercent(const float TimeRemaining, const float CooldownDuration)
+{
+	return (CooldownDuration - TimeRemaining) / CooldownDuration;
+}
---
+++ Source/Radiant/UI/RTInfoPanel.h
@@ -0,0 +1,112 @@
+// Copyright Radiant Studios
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/Image.h"
+#include "RTInfoPanel.generated.h"
+
+UENUM(BlueprintType)
+enum class EAbilityID : uint8
+{
+	Ability1		UMETA(DisplayName="Ability1"),
+	Ability2		UMETA(DisplayName="Ability2"),
+	Ability3		UMETA(DisplayName="Ability3"),
+	Ability4		UMETA(DisplayName="Ability4"),
+	Ability5		UMETA(DisplayName="Ability5"),
+	Ability6		UMETA(DisplayName="Ability6")
+};
+
+USTRUCT()
+struct FUIAbility
+{
+	GENERATED_BODY()
+	uint8 bOn : 1;
+	UMaterialInstanceDynamic* MaterialInstance{};
+	class UImage* AbilityImage{};
+	class UImage* MaskImage{};
+
+	FUIAbility(UMaterialInterface* Interface = nullptr, UObject* Context = nullptr, UImage* Ability = nullptr, UImage* Mask = nullptr)
+		: AbilityImage(Ability), MaskImage(Mask)
+	{
+		MaterialInstance = UMaterialInstanceDynamic::Create(Interface, Context);
+		bOn = false;
+	}
+
+	void SetOn(bool On)
+	{
+		bOn = On;
+		if(bOn)
+			AbilityImage->SetColorAndOpacity(FLinearColor::Gray);
+		else
+			AbilityImage->SetColorAndOpacity(FLinearColor::White);
+		MaskImage->SetIsEnabled(On);
+	}
+};
+
+/**
+ * 
+ */
+UCLASS()
+class RADIANT_API URTInfoPanel : public UUserWidget
+{
+	GENERATED_BODY()
+
+	UPROPERTY(meta=(BindWidget))
+	class UImage* Ability1;
+	UPROPERTY(meta=(BindWidget))
+	class UImage* Ability1CDMask;
+
+	UPROPERTY(meta=(BindWidget))
+	class UImage* Ability2;
+	UPROPERTY(meta=(BindWidget))
+	class UImage* Ability2CDMask;
+	
+	UPROPERTY(meta=(BindWidget))
+	class UImage* Ability3;
+	UPROPERTY(meta=(BindWidget))
+	class UImage* Ability3CDMask;
+	
+	UPROPERTY(meta=(BindWidget))
+	class UImage* Ability4;
+	UPROPERTY(meta=(BindWidget))
+	class UImage* Ability4CDMask;
+
+	UPROPERTY(meta=(BindWidget))
+	class UImage* Ability5;
+	UPROPERTY(meta=(BindWidget))
+	class UImage* Ability5CDMask;
+	
+	UPROPERTY(meta=(BindWidget))
+	class UImage* Ability6;
+	UPROPERTY(meta=(BindWidget))
+	class UImage* Ability6CDMask;
+
+	UPROPERTY(EditAnywhere)
+	class UMaterialInterface* Mat;
+	
+	UPROPERTY(meta=(BindWidget))
+	class UProgressBar* HealthBar;
+	UPROPERTY(meta=(BindWidget))
+	class UProgressBar* ManaBar;
+
+	virtual void NativeTick(const FGeometry& MyGeometry, float DeltaTime) override;
+	
+	TArray<FUIAbility> Abilities;
+	UPROPERTY(EditAnywhere, Category="Cooldowns")
+	TArray<FGameplayTagContainer> Tags;
+public:
+	void SetAbilityCoolDown(EAbilityID Abilty, const float Percent);
+	void AbilityCooldowns();
+	void UpdateProperties(float DeltaTime);
+
+	UFUNCTION(BlueprintCallable)
+	void Init();
+
+	bool GetCooldownRemainingForTag(FGameplayTagContainer CooldownTags, float& TimeRemaining, float& CooldownDuration);
+
+	float GetCooldownPercent(const float TimeRemaining, const float CooldownDuration);
+
+};
