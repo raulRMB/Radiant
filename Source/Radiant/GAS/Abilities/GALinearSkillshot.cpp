32dad9d5b1b43257de0bfd517f7f99c5a6cd6bd1
raulRMB <92171903+raulRMB@users.noreply.github.com>
2023-05-29 19:31:07 -0400

    Projectile WIP

--- Source/Radiant/GAS/Abilities/GALinearSkillshot.cpp
+++ Source/Radiant/GAS/Abilities/GALinearSkillshot.cpp
@@ -4,11 +4,9 @@
 #include "GAS/Abilities/GALinearSkillshot.h"
 
 #include "Character/Hero.h"
+#include "AbilitySystemBlueprintLibrary.h"
 #include "Player/RTPlayerState.h"
-#include "Util/MouseTargetActor.h"
-#include "Abilities/Tasks/AbilityTask_WaitTargetData.h"
-#include "Util/MouseVec.h"
-#include "Util/Util.h"
+#include "Util/PlayMontageAndWaitForEvent.h"
 
 UGALinearSkillshot::UGALinearSkillshot()
 {
@@ -27,4 +25,54 @@ void UGALinearSkillshot::ActivateAbility(const FGameplayAbilitySpecHandle Handle
 	AHero* Avatar = Cast<AHero>(GetAvatarActorFromActorInfo());
 	ARTPlayerState* Owner = Cast<ARTPlayerState>(GetOwningActorFromActorInfo());
 
+	FHitResult HitResult = UAbilitySystemBlueprintLibrary::GetHitResultFromTargetData(TriggerEventData->TargetData,0);
+
+	FVector Loc = Avatar->GetActorLocation();
+	Loc.Z = 0;
+	FVector Direction = (HitResult.Location - Loc).GetSafeNormal();
+	Avatar->SetRotationLock(true, Direction);
+
+	MouseWorldLocation = HitResult.Location;
+
+	UPlayMontageAndWaitForEvent* Task = UPlayMontageAndWaitForEvent::PlayMontageAndWaitForEvent(this, NAME_None, Montage, FGameplayTagContainer(), 1.0f, NAME_None, false, 1.0f);
+	// UPlayMontageAndWaitForEvent* Task = UPlayMontageAndWaitForEvent::PlayMontageAndWaitForEvent(this, FName("Fireball"), Montage, FGameplayTagContainer(), MontagePlayRate);
+	Task->OnCompleted.AddDynamic(this, &UGALinearSkillshot::OnCompleted);
+	Task->OnCancelled.AddDynamic(this, &UGALinearSkillshot::OnCancelled);
+	Task->OnInterrupted.AddDynamic(this, &UGALinearSkillshot::OnInterrupted);
+	Task->OnBlendOut.AddDynamic(this, &UGALinearSkillshot::OnBlendOut);
+	Task->EventReceived.AddDynamic(this, &UGALinearSkillshot::OnEventReceived);
+	Task->ReadyForActivation();
+}
+
+void UGALinearSkillshot::OnCompleted(FGameplayTag EventTag, FGameplayEventData EventData)
+{
+}
+
+void UGALinearSkillshot::OnCancelled(FGameplayTag EventTag, FGameplayEventData EventData)
+{
+}
+
+void UGALinearSkillshot::OnInterrupted(FGameplayTag EventTag, FGameplayEventData EventData)
+{
+}
+
+void UGALinearSkillshot::OnBlendOut(FGameplayTag EventTag, FGameplayEventData EventData)
+{
+}
+
+void UGALinearSkillshot::OnEventReceived(FGameplayTag EventTag, FGameplayEventData EventData)
+{
+	if(HasAuthority(&CurrentActivationInfo))
+	{
+		CommitAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo);
+
+		AActor* Avatar = GetAvatarActorFromActorInfo();
+		FVector SpawnLocation = Avatar->GetActorLocation() + Avatar->GetActorForwardVector() * SpawnRange;
+		FVector TargetLocation = MouseWorldLocation;
+		TargetLocation.Z = Avatar->GetActorLocation().Z;
+		FRotator Rotation = (TargetLocation - Avatar->GetActorLocation()).Rotation();
+		FTransform SpawnTransform = FTransform(Rotation, SpawnLocation);
+		
+		GetWorld()->SpawnActor(SpawnActor, &SpawnTransform);
+	}
 }
--- Source/Radiant/GAS/Abilities/GALinearSkillshot.h
+++ Source/Radiant/GAS/Abilities/GALinearSkillshot.h
@@ -16,8 +16,36 @@ class RADIANT_API UGALinearSkillshot : public UGameplayAbility
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ability", meta = (AllowPrivateAccess = "true"))
 	class AActor* Instigator;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ability", meta = (AllowPrivateAccess = "true"))
+	class UAnimMontage* Montage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ability", meta = (AllowPrivateAccess = "true"))
+	float MontagePlayRate = 1.f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ability", meta = (AllowPrivateAccess = "true"))
+	FGameplayTagContainer SpawnTags;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ability", meta = (AllowPrivateAccess = "true"))
+	TSubclassOf<AActor> SpawnActor;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ability", meta = (AllowPrivateAccess = "true"))
+	float SpawnRange = 300.f;
+private:
+	FVector MouseWorldLocation;
 public:
 	UGALinearSkillshot();
 	
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+	UFUNCTION()
+	void OnCompleted(FGameplayTag EventTag, FGameplayEventData EventData);
+	UFUNCTION()
+	void OnCancelled(FGameplayTag EventTag, FGameplayEventData EventData);
+	UFUNCTION()
+	void OnInterrupted(FGameplayTag EventTag, FGameplayEventData EventData);
+	UFUNCTION()
+	void OnBlendOut(FGameplayTag EventTag, FGameplayEventData EventData);
+	UFUNCTION()
+	void OnEventReceived(FGameplayTag EventTag, FGameplayEventData EventData);
 };
