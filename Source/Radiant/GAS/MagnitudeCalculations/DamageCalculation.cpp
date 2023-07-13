153db9967060bbca71a9f21a0ac40bf56cf6fe54
Mike <MashDevel@users.noreply.github.com>
2023-07-13 14:43:58 -0400

    invulnerable ability

--- Source/Radiant/GAS/MagnitudeCalculations/DamageCalculation.cpp
+++ Source/Radiant/GAS/MagnitudeCalculations/DamageCalculation.cpp
@@ -17,12 +17,17 @@ float UDamageCalculation::CalculateBaseMagnitude_Implementation(const FGameplayE
 {
 	// Gather the tags from the source and target as that can affect which buffs should be used
 	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
-	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = &Spec.CapturedTargetTags.GetActorTags();
 
 	FAggregatorEvaluateParameters EvaluationParameters;
 	EvaluationParameters.SourceTags = SourceTags;
 	EvaluationParameters.TargetTags = TargetTags;
-
+	
+	if(TargetTags->HasTag(FGameplayTag::RequestGameplayTag(FName("States.Invulnerable"))))
+	{
+		return 0.f;
+	}
+	
 	float Damage = 0.f;
 	GetCapturedAttributeMagnitude(DamageDef, Spec, EvaluationParameters, Damage);
 	Damage = FMath::Max<float>(Damage, 0.0f);
