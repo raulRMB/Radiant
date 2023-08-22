5926eb15dcb0fb226bc57773c0d5c90e55b83ae8
raulRMB <92171903+raulRMB@users.noreply.github.com>
2023-08-22 16:38:06 -0400

    Arena hitbox

---
+++ Source/Radiant/Util/MapHitboxes/MapHitBox.cpp
@@ -0,0 +1,59 @@
+﻿// Copyright Radiant Studios
+
+
+#include "MapHitBox.h"
+#include "Components/CapsuleComponent.h"
+#include "Characters/RTCharacter.h"
+#include "Util/Util.h"
+
+
+// Sets default values
+AMapHitBox::AMapHitBox()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CapsuleComponent = CreateDefaultSubobject<UCapsuleComponent>("CapsuleComponent");
+	SetRootComponent(CapsuleComponent);
+}
+
+// Called when the game starts or when spawned
+void AMapHitBox::BeginPlay()
+{
+	Super::BeginPlay();
+
+	CapsuleComponent->OnComponentEndOverlap.AddDynamic(this, &AMapHitBox::OnEndOverlap);
+}
+
+void AMapHitBox::OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if(ARTCharacter* Character = Cast<ARTCharacter>(OtherActor))
+	{
+		FVector ToCenter = Character->GetActorLocation() - GetActorLocation();
+		FVector Dir = ToCenter.GetSafeNormal() * (CapsuleComponent->GetScaledCapsuleRadius() + Character->GetCapsuleComponent()->GetScaledCapsuleRadius());
+		Character->SetActorLocation(Dir + GetActorLocation());
+	}
+}
+
+// Called every frame
+void AMapHitBox::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	/* for(ARTCharacter* Character : Characters)
+	{
+		if(IsValid(Character))
+		{
+			FVector CharacterLocation = Character->GetActorLocation();
+			FVector Dir = GetActorLocation() - CharacterLocation;
+			Dir.Normalize();
+			Character->SetActorLocation(CharacterLocation + Dir * MoveSpeed *  DeltaTime);
+		}
+		else
+		{
+			Characters.Remove(Character);
+		}
+	} */
+}
+
---
+++ Source/Radiant/Util/MapHitboxes/MapHitBox.h
@@ -0,0 +1,36 @@
+﻿// Copyright Radiant Studios
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MapHitBox.generated.h"
+
+class ARTCharacter;
+
+UCLASS()
+class RADIANT_API AMapHitBox : public AActor
+{
+	GENERATED_BODY()
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	class UCapsuleComponent* CapsuleComponent;
+
+	UPROPERTY()
+	TArray<ARTCharacter*> Characters;
+
+public:
+	// Sets default values for this actor's properties
+	AMapHitBox();
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	UFUNCTION()
+	void OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	                  UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
