6b191338e49e0bb786a0634d95c61b94a757f55c
Mike <MashDevel@users.noreply.github.com>
2023-08-29 17:07:06 -0400

    fix pickup item

--- Source/Radiant/Util/Interfaces/Carrier.cpp
+++ Source/Radiant/Util/Interfaces/Carrier.cpp
@@ -8,8 +8,7 @@
 // Add default functionality here for any ICarrier functions that are not pure virtual.
 void ICarrier::PickUpItem(const FName& ItemName, const uint32 Amount)
 {
-	FItemData* ItemData = UUtil::GetItemDataFromName(ItemName);
-	GetInventory()->AddItem(ItemName, ItemData, Amount);
+	GetInventory()->AddItemUnchecked(ItemName, Amount);
 }
 
 void ICarrier::DropItem(const FName& ItemName)
