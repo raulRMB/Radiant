f60435218c3ad93e580db24abda91efb07d28129
Mike <MashDevel@users.noreply.github.com>
2023-09-16 21:18:28 -0400

    styles

--- Gluu/Platform/Launcher/src/renderer/src/Login.jsx
+++ Gluu/Platform/Launcher/src/renderer/src/Login.jsx
@@ -15,29 +15,31 @@ export const Login = ({ socket, setRegisterOpen }) => {
   }
 
   return (
-  <div className="w-full bg-gray-800 h-screen flex items-center justify-center flex-col">
+  <div className="w-full bg-black h-screen flex items-center justify-center flex-col">
     <div className='w-1/3 flex items-center justify-center flex-col'>
     <h2 className="text-white text-6xl p-4">Radiant</h2>
-    <form method="post" className="w-full bg-white shadow-md rounded px-8 pt-6 pb-4 mb-4" onSubmit={handleSubmit}>
+    <form method="post" className="w-full bg-gray-700 text-white shadow-md rounded px-8 pt-6 pb-4 mb-4" onSubmit={handleSubmit}>
       <div className="mb-4">
-        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+        <label className="block text-white text-sm font-bold mb-2" htmlFor="username">
           Username
         </label>
-        <input name="username" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
+        <input name="username" className="shadow appearance-none rounded bg-gray-800 w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
       </div>
       <div className="mb-6">
-        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+        <label className="block text-white text-sm font-bold mb-2" htmlFor="password">
           Password
         </label>
-        <input name="password" className="shadow appearance-none rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
+        <input name="password" className="shadow appearance-none rounded bg-gray-800 w-full py-2 px-3 text-white mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
       </div>
       <div className="flex items-center justify-between">
-        <button type="submit" className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+        <button type="submit" className="w-full bg-blue-700 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
           Login
         </button>
       </div>
     </form>
-    <p className='text-right text-blue-500 w-full hover:text-blue-300 hover:cursor-pointer' onClick={() => setRegisterOpen(true)}>Register Account?</p>
+    <div className='w-full'>
+      <p className='text-right text-blue-500 hover:text-blue-300 hover:cursor-pointer' onClick={() => setRegisterOpen(true)}>Register Account?</p>
+    </div>
     </div>
   </div>
   );
--- Gluu/Platform/Launcher/src/renderer/src/Register.jsx
+++ Gluu/Platform/Launcher/src/renderer/src/Register.jsx
@@ -15,24 +15,24 @@ export const Register = ({ socket, setRegisterOpen }) => {
   }
 
   return (
-  <div className="w-full bg-gray-800 h-screen flex items-center justify-center flex-col">
+  <div className="w-full bg-black h-screen flex items-center justify-center flex-col">
     <div className='w-1/3 flex items-center justify-center flex-col'>
     <h2 className="text-white text-6xl p-4">Register Account</h2>
-    <form method="post" className="w-full bg-white shadow-md rounded px-8 pt-6 pb-4 mb-4" onSubmit={handleSubmit}>
+    <form method="post" className="w-full bg-gray-700 shadow-md rounded px-8 pt-6 pb-4 mb-4" onSubmit={handleSubmit}>
       <div className="mb-4">
-        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+        <label className="block text-white text-sm font-bold mb-2" htmlFor="username">
           Username
         </label>
-        <input name="username" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
+        <input name="username" className="shadow appearance-none rounded w-full bg-gray-800 py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
       </div>
       <div className="mb-6">
-        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+        <label className="block text-white text-sm font-bold mb-2" htmlFor="password">
           Password
         </label>
-        <input name="password" className="shadow appearance-none rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
+        <input name="password" className="shadow appearance-none rounded w-full bg-gray-800 py-2 px-3 text-white mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
       </div>
       <div className="flex items-center justify-between">
-        <button type="submit" className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+        <button type="submit" className="w-full bg-blue-700 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
           Register
         </button>
       </div>
